4ac52f467b7a0e1c7d078d9d17d050a754f00ab3
valtyr <bloomquist.robert@gmail.com>
2011-12-10 15:29:58 -0500

    cleaned up various bits

--- fft/fft.go
+++ fft/fft.go
@@ -17,8 +17,8 @@
 package fft
 
 import (
+	"errors"
 	"math"
-	"os"
 )
 
 // radix-2 factors
@@ -29,43 +29,6 @@ var n2_factors = map[int][]complex128{}
 var n2_inv_factors = map[int][]complex128{}
 var n2_conj_factors = map[int][]complex128{}
 
-// Ensures the complex multiplication factors exist for an input array of length input_len.
-func ensureFactors(input_len int) {
-	var cos, sin float64
-
-	for i := 4; i <= input_len; i <<= 1 {
-		if _, present := factors[i]; !present {
-			factors[i] = make([]complex128, i)
-			for n := 0; n < i; n++ {
-				if n == 0 {
-					sin, cos = 0, 1
-				} else if n*4 == i {
-					sin, cos = -1, 0
-				} else {
-					sin, cos = math.Sincos(-2 * math.Pi / float64(i) * float64(n))
-				}
-				factors[i][n] = complex(cos, sin)
-			}
-		}
-	}
-
-	if _, present := n2_factors[input_len]; !present {
-		n2_factors[input_len] = make([]complex128, input_len)
-		n2_inv_factors[input_len] = make([]complex128, input_len)
-		n2_conj_factors[input_len] = make([]complex128, input_len)
-
-		for i := 0; i < input_len; i++ {
-			if i == 0 {
-				sin, cos = 0, 1
-			} else {
-				sin, cos = math.Sincos(math.Pi / float64(input_len) * math.Pow(float64(i), 2))
-			}
-			n2_factors[input_len][i] = complex(cos, sin)
-			n2_inv_factors[input_len][i] = complex(cos, -sin)
-		}
-	}
-}
-
 func FFT_real(x []float64) []complex128 {
 	return FFT(ToComplex(x))
 }
@@ -74,6 +37,7 @@ func IFFT_real(x []float64) []complex128 {
 	return IFFT(ToComplex(x))
 }
 
+// ToComplex takes a []float64 and converts it to []complex128
 func ToComplex(x []float64) []complex128 {
 	y := make([]complex128, len(x))
 	for n, v := range x {
@@ -82,10 +46,7 @@ func ToComplex(x []float64) []complex128 {
 	return y
 }
 
-func FFT(x []complex128) []complex128 {
-	return computeFFT(x)
-}
-
+// IFFT calculates the inverse fast Fourier transform of x.
 func IFFT(x []complex128) []complex128 {
 	lx := len(x)
 	r := make([]complex128, lx)
@@ -93,10 +54,10 @@ func IFFT(x []complex128) []complex128 {
 	// Reverse inputs, which is calculated with modulo N, hence x[0] as an outlier
 	r[0] = x[0]
 	for i := 1; i < lx; i++ {
-		r[i] = x[lx - i]
+		r[i] = x[lx-i]
 	}
 
-	r = computeFFT(r)
+	r = FFT(r)
 
 	N := complex(float64(lx), 0)
 	for n, _ := range r {
@@ -105,9 +66,9 @@ func IFFT(x []complex128) []complex128 {
 	return r
 }
 
-func Convolve(x, y []complex128) ([]complex128, os.Error) {
+func Convolve(x, y []complex128) ([]complex128, error) {
 	if len(x) != len(y) {
-		return []complex128{}, os.NewError("fft: input arrays are not of equal length")
+		return []complex128{}, errors.New("fft: input arrays are not of equal length")
 	}
 
 	fft_x := FFT(x)
@@ -121,47 +82,50 @@ func Convolve(x, y []complex128) ([]complex128, os.Error) {
 	return IFFT(r), nil
 }
 
-func computeFFT(x []complex128) []complex128 {
-	var r []complex128
-	lx := len(x)
+// FFT calculates the fast Fourier transform of x.
+func FFT(x []complex128) []complex128 {
+	l := len(x)
 
-	// todo: non-hack handling length <= 1 cases
-	if lx <= 1 {
-		r = make([]complex128, lx)
-		for n, v := range x {
-			r[n] = v
-		}
+	// TODO: non-hack handling length <= 1 cases.
+	if l <= 1 {
+		r := make([]complex128, l)
+		copy(r,x)
 		return r
 	}
-
-	if IsPowerOf2(lx) {
-		r = Radix2FFT(x)
-	} else {
-		r = BluesteinFFT(x)
+	if IsPowerOf2(l) {
+		return Radix2FFT(x)
 	}
-
-	return r
+	return BluesteinFFT(x)
 }
 
 func Radix2FFT(x []complex128) []complex128 {
 	lx := len(x)
-	ensureFactors(lx)
+
+	for i := 4; i <= lx; i <<= 1 {
+		if _, present := factors[i]; !present {
+			factors[i] = make([]complex128, i)
+			for n := 0; n < i; n++ {
+				sin, cos := math.Sincos(-2 * float64(n) * math.Pi / float64(i))
+				factors[i][n] = complex(cos, sin)
+			}
+		}
+	}
 
 	lx_2 := lx / 2
 	r := make([]complex128, lx) // result
-	t := make([]complex128, lx) // temp
+	t := make([]complex128, lx)	// temp
 	copy(r, x)
 
 	// split into even and odd parts for each stage
-	for block_sz := lx; block_sz > 1; block_sz >>= 1 {
+	for b := lx; b > 1; b >>= 1 {
 		i := 0
-		bs_2 := block_sz / 2
-		for block := 0; block < lx/block_sz; block++ {
-			for n := 0; n < bs_2; n++ {
-				bn := block_sz*block + n
+		b_2 := b / 2
+		for blk := 0; blk < lx/b; blk++ {
+			for n := 0; n < b_2; n++ {
+				bn := b*blk + n
 				t[bn] = r[i]
 				i++
-				t[bn+bs_2] = r[i]
+				t[bn+b_2] = r[i]
 				i++
 			}
 		}
@@ -169,12 +133,14 @@ func Radix2FFT(x []complex128) []complex128 {
 	}
 
 	for stage := 2; stage <= lx; stage <<= 1 {
-		if stage == 2 { // 2-point transforms
+		if stage == 2 {
+			// 2-point transforms
 			for n := 0; n < lx_2; n++ {
 				t[n*2] = r[n*2] + r[n*2+1]
 				t[n*2+1] = r[n*2] - r[n*2+1]
 			}
-		} else { // >2-point transforms
+		} else {
+			// >2-point transforms
 			blocks := lx / stage
 			s_2 := stage / 2
 
@@ -190,18 +156,28 @@ func Radix2FFT(x []complex128) []complex128 {
 
 		copy(r, t)
 	}
-
 	return r
 }
 
 func BluesteinFFT(x []complex128) []complex128 {
 	lx := len(x)
-	a := ZeroPad(x, NextPowerOf2(lx * 2 - 1))
+	a := ZeroPad(x, NextPowerOf2(lx*2-1))
 	la := len(a)
-	ensureFactors(lx)
 
-	for n, v := range x {
-		a[n] = v * n2_inv_factors[lx][n]
+	if _, present := n2_factors[lx]; !present {
+		n2_factors[lx] = make([]complex128, lx)
+		n2_inv_factors[lx] = make([]complex128, lx)
+		n2_conj_factors[lx] = make([]complex128, lx)
+
+		for i := 0; i < lx; i++ {
+			sin, cos := math.Sincos(math.Pi / float64(lx) * float64(i*i))
+			n2_factors[lx][i] = complex(cos, sin)
+			n2_inv_factors[lx][i] = complex(cos, -sin)
+		}
+	}
+
+	for i, v := range x {
+		a[i] = v * n2_inv_factors[lx][i]
 	}
 
 	b := make([]complex128, la)
@@ -210,48 +186,49 @@ func BluesteinFFT(x []complex128) []complex128 {
 			b[i] = n2_factors[lx][i]
 		} else if i < lx {
 			b[i] = n2_factors[lx][i]
-			b[la - i] = n2_factors[lx][i]
+			b[la-i] = n2_factors[lx][i]
 		}
 	}
 
+	// NOTE: The only error Convolve returns is if a and
+	// b are not the same length.  Here, b is initialized
+	// to len(a), so it's okay to ignore the error.
 	r, _ := Convolve(a, b)
 
 	for i := 0; i < lx; i++ {
-		r[i] = n2_inv_factors[lx][i] * r[i]
+		r[i] *= n2_inv_factors[lx][i]
 	}
 
 	return r[:lx]
 }
 
+// IsPowerOf2 returns whether or not x is a power of 2.
 func IsPowerOf2(x int) bool {
-	return x & (x - 1) == 0
+	return x&(x-1) == 0
 }
 
-// Returns the next power of 2 >= x.
+// NextPowerOf2 returns the smallest power
+// of 2 greater than or equal to x.
 func NextPowerOf2(x int) int {
-	if !IsPowerOf2(x) {
-		x = int(math.Pow(2, math.Ceil(math.Log2(float64(x)))))
+	if IsPowerOf2(x) {
+		return x
 	}
-
-	return x
+	return int(math.Pow(2, math.Ceil(math.Log2(float64(x)))))
 }
 
+// ZeroPad appends zero values to x to the specified length.
 func ZeroPad(x []complex128, length int) []complex128 {
-	lx := len(x)
-
-	if len(x) != length {
-		r := make([]complex128, length)
-		copy(r, x)
-		for i := 0; i < length - lx; i++ {
-			r[i + lx] = 0
-		}
-		x = r
+	if len(x) == length {
+		return x
 	}
-
-	return x
+	r := make([]complex128, length)
+	copy(r, x)
+	return r
 }
 
-// Zero pads to the next power of 2 >= len(x).
+// ZeroPad2 calls ZeroPad with the length set to
+// the next power of 2 greater than or equal to x.
 func ZeroPad2(x []complex128) []complex128 {
 	return ZeroPad(x, NextPowerOf2(len(x)))
 }
+
